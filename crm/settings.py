6184b27328698ae58ea118cd4954a0fce8549e56
Rohan Devasthale <rdevasthale98@gmail.com>
2024-01-31 17:45:31 -0500

    Password change

--- crm/settings.py
+++ crm/settings.py
@@ -72,7 +72,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'crmpro',
         'USER': 'root',
-        'PASSWORD': 'RoSD04031998@',
+        'PASSWORD': '###########',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
